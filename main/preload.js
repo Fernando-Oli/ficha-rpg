18b7abe72fa987d657df332056505f60634f3a76
Fernando-Oli <primoflro@gmail.com>
2025-08-06 01:41:57 -0300

    fix: remove elements for the better live implementation in the workshop to make a template for the lecture

--- main/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld("electron", {
-  saveSheet: (sheet) => ipcRenderer.invoke("save-sheet", sheet),
-  listarFichas: () => ipcRenderer.invoke('listar-fichas')
-});
